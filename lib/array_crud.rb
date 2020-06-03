699c44390d72b7364cfcc0f06e9135c2b5c88cf4
Uche Nweke Jr <unwekejr@gmail.com>
2020-06-03 00:37:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,7 +27,8 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array7 = ["elements", "are", "cool", "I", "guess"]
+  array7[]
 end
 
 def retrieve_first_element_from_array(array)
